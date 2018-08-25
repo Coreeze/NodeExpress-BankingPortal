0a06029667824917bf1ba873229ab376f5af2828
Coreeze <cristianlenta@yahoo.com>
2018-08-25 16:18:52 +0200

    Completed the module

--- src/app.js
+++ src/app.js
@@ -7,16 +7,6 @@ const app = express();
 
 const {accounts, users, writeJSON} = require('/data');
 
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
